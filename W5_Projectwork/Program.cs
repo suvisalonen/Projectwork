4095dcdc8a2430c3035819c957a95bc2e2e3c4f4
idvele <ilari.katajamaki1@gmail.com>
2022-07-05 10:00:58 +0300

    making the first skeleton

--- W5_Projectwork/Program.cs
+++ W5_Projectwork/Program.cs
@@ -6,7 +6,50 @@ namespace W5_Projectwork
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Tervetuloa tapahtumahakuun");
+
+            Console.WriteLine("");
+
+
+
+        }
+
+        public class Input
+        {
+
+            public static void menuSelectionLogic()
+            {
+               
+
+                bool i = true;
+                while (i)
+                {
+
+                    if (input == "1")
+                    {
+                        //events
+                        i = false;
+                    }
+
+                    else if (input == "2")
+                    {
+                        //activities
+                        i = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("ERROR!");
+                    }
+                }
+
+            }
+           
+	
+
+
+	
+
+
         }
     }
 }
