c71d1906ddab23c509498af6c1ce25e8d06a6a68
Sampsa Vaurio <sampsa.vaurio@gmail.com>
2022-07-06 10:36:16 +0300

    Added coordinate update to event DTO

--- W5_Projectwork/EventsDataTransferObj.cs
+++ W5_Projectwork/EventsDataTransferObj.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace W5_Projectwork
 {
@@ -12,5 +13,17 @@ namespace W5_Projectwork
         public string FilterTags { get; set; }
         public DateTime FilterDate { get; set; }
 
+        public HelsinkiEventDataTransferObject() 
+        { 
+        }
+
+        public async Task UpdateCoordinates() 
+        {
+            Dictionary<string, double> coordinates = await GeoCoordinatesUtil.GetGeoCoordinatesAsync(PostalCode);
+
+            this.lat = coordinates["lat"];
+            this.lon = coordinates["lon"];
+        }
     }
+
 }
